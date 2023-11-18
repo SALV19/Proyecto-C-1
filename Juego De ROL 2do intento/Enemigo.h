9519623ddc5c8592f009f158d89c41fb3f49cde2
SALV <145412063+SALV19@users.noreply.github.com>
2023-11-18 16:12:30 -0600

    Add files via upload

---
+++ Juego De ROL 2do intento/Enemigo.h
@@ -0,0 +1,32 @@
+#ifndef ENEMIGO_H_INCLUDED
+#define ENEMIGO_H_INCLUDED
+
+#endif // ENEMIGO_H_INCLUDED
+
+class Enemigo : public Persona{
+private:
+    string tipo;
+    int cantidad;
+public:
+    Enemigo(string, int, int);
+    Enemigo(string, int);
+
+    void setTipo(string);
+    void setCantidad(int);
+
+    string getTipo();
+    int getCantidad();
+};
+
+Enemigo::Enemigo(string _nombre, int _nivel, int _vida) : Persona(_nombre, _nivel, _vida){}
+
+
+void Enemigo::setTipo(string _tipo){
+    tipo = _tipo;}
+void Enemigo::setCantidad(int _cantidad){
+    cantidad = _cantidad;}
+
+string Enemigo::getTipo(){
+    return tipo;}
+int Enemigo::getCantidad(){
+    return cantidad;}
